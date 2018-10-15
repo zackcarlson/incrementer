a2dc9c7e73b406e06528d91b6e1b5b641ea8e81b
zackcarlson <zackt.carlson@gmail.com>
2018-10-15 12:57:01 -0700

    Import css styles

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './container/App.js';
+import '../styles.css';
 
 ReactDOM.render(
   <BrowserRouter>
