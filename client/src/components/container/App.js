534d68b6d523937b67d7d23e61e7a71b13f9b6b2
zackcarlson <zackt.carlson@gmail.com>
2018-10-15 12:56:43 -0700

    Create App container and outline server functions/view logic

--- client/src/components/container/App.js
+++ client/src/components/container/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import Home from './AuthorizedHome';
 
 class App extends Component {
+  // check if token exists 
+    // render authorized home view 
+    // else render signin/signup page
+
+  // post to signin and post to signup
+    // render authorized home view upon successful log in/signup, 
+    // else render signin/signup view with error message
+  
   render() {
     return (
-      <div>APP CONTAINER</div>
+      <div>
+        APP CONTAINER
+        <AuthorizedHome />
+      </div>
     );
   }
 }
