b440dcd238e49ea09bd3226089e6a3113d293cd2
zackcarlson <zackt.carlson@gmail.com>
2018-10-16 22:21:32 -0700

    Use * all routes

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 const public = '/../client/src/';
 
-router.get('/', (req, res) => {
+router.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname + public + 'index.html'));
 });
 
