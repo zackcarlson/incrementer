29c01170885d4f9bab8fad7aceb1fbe3906f2715
zackcarlson <zackt.carlson@gmail.com>
2018-10-16 22:21:56 -0700

    Create login template

---
+++ client/src/components/container/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Login = function() {
+  return (
+    <section className="login-container">
+      <div className="login-form">
+          <form action="" method="post">
+            <h2 className="text-center">Sign in</h2>
+
+            <div className="form-group">
+              <div className="input-group">
+                <input type="text" className="form-control" name="username" placeholder="Username" required="required"/>				
+              </div>
+            </div>
+
+            <div className="form-group">
+              <div className="input-group">
+                <input type="password" className="form-control" name="password" placeholder="Password" required="required"/>				
+              </div>
+            </div>  
+
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary login-btn btn-block">Sign in</button>
+            </div>
+          </form>
+
+          <div className="or-seperator"><i>or</i></div>
+          <div className="form-group">
+          <NavLink exact to="/signup"><button type="submit" className="btn btn-primary signup-btn btn-block">Sign up</button></NavLink>
+          </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
