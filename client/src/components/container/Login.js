47004c02c7d87c907cb9695bcf34a76570f88ffd
zackcarlson <zackt.carlson@gmail.com>
2018-10-17 19:14:07 -0700

    Make class component and add browser history

--- client/src/components/container/Login.js
+++ client/src/components/container/Login.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Login = function() {
-  return (
-    <section className="login-container">
-      <div className="login-form">
-          <form action="" method="post">
-            <h2 className="text-center">Sign in</h2>
+class Login extends Component {
 
-            <div className="form-group">
-              <div className="input-group">
-                <input type="text" className="form-control" name="username" placeholder="Username" required="required"/>				
+  componentWillMount() {
+    this.props.history.push(this.props.history.location.pathname);
+  }
+
+  render() {
+    return (
+      <section className="login-container">
+        <div className="login-form">
+            <form action="" method="post">
+              <h2 className="text-center">Sign in</h2>
+
+              <div className="form-group">
+                <div className="input-group">
+                  <input type="text" className="form-control" name="username" placeholder="Username" required="required"/>				
+                </div>
               </div>
-            </div>
 
-            <div className="form-group">
-              <div className="input-group">
-                <input type="password" className="form-control" name="password" placeholder="Password" required="required"/>				
+              <div className="form-group">
+                <div className="input-group">
+                  <input type="password" className="form-control" name="password" placeholder="Password" required="required"/>				
+                </div>
+              </div>  
+
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary login-btn btn-block">Sign in</button>
               </div>
-            </div>  
+            </form>
 
+            <div className="or-seperator"><i>or</i></div>
             <div className="form-group">
-              <button type="submit" className="btn btn-primary login-btn btn-block">Sign in</button>
+            <NavLink exact to="/signup" style={{textDecoration: 'none'}}><button type="submit" className="btn btn-primary signup-btn btn-block">Sign up</button></NavLink>
             </div>
-          </form>
-
-          <div className="or-seperator"><i>or</i></div>
-          <div className="form-group">
-          <NavLink exact to="/signup"><button type="submit" className="btn btn-primary signup-btn btn-block">Sign up</button></NavLink>
-          </div>
-      </div>
-    </section>
-  );
-};
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Login;
