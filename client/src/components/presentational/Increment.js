9ae274e99bb9e21142717c6229054036dee09a9e
zackcarlson <zackt.carlson@gmail.com>
2018-10-17 19:15:57 -0700

    Make class component and incorporate modal hide and show functionality

--- client/src/components/presentational/Increment.js
+++ client/src/components/presentational/Increment.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Modal from './Modal';
 
-const Increment = function() {
-  return <button>Increment</button>;
-};
+class Increment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false, currVal: 0 };
+    this.hideModal = this.hideModal.bind(this);
+    this.showModal = this.showModal.bind(this);
+  }
+
+  showModal() {
+    this.setState({ show: true });
+  }
+
+  hideModal() {
+    this.setState({ show: false });
+  }
+
+  render() {
+    return (
+      <main>
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <p>Modal</p>
+          <p>Data</p>
+        </Modal>
+        <button type="button" onClick={this.showModal}>
+          open
+        </button>
+      </main>
+    );
+  }
+}
 
 export default Increment;
