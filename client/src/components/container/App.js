86db14ea0db3f844048943e66d576ee4020f1fbe
zackcarlson <zackt.carlson@gmail.com>
2018-10-17 19:12:31 -0700

    Incorporate react router and browser history

--- client/src/components/container/App.js
+++ client/src/components/container/App.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AuthorizedHome from './AuthorizedHome';
 import Navbar from './Navbar';
 import Login from './Login';
 import SignUp from './Signup';
 
 class App extends Component {
-  // check if token exists 
-    // render authorized home view 
-    // else render signin/signup page
 
-  // post to signin and post to signup
-    // render authorized home view upon successful log in/signup, 
-    // else render signin/signup view with error message
-  
   render() {
     return (
-      <div>
-        <Navbar/>
-        <Switch>
-          <Route exact path='/' component={Login}/>
-          <Route path='/signup' component={SignUp}/>
-          <Route path='/home/:userId' component={AuthorizedHome}/>
-        </Switch> 
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' exact render={props => <Login {...props}/>} />
+            <Route path='/signup' exact render={props => <SignUp {...props}/>} />
+            <Route path='/home' exact render={props => <AuthorizedHome {...props}/>} />
+          </Switch> 
+        </div>
+      </BrowserRouter>
     );
   }
+
 }
 
 export default App;
