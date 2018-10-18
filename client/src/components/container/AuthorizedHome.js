426142ec64d8ef7dcf3158dee0007a0948dee165
zackcarlson <zackt.carlson@gmail.com>
2018-10-17 19:13:03 -0700

    Add browser history to home

--- client/src/components/container/AuthorizedHome.js
+++ client/src/components/container/AuthorizedHome.js
@@ -9,6 +9,15 @@ class AuthorizedHome extends Component {
       currVal: 0
     };
   }
+
+  // post function to initially do math 
+  // post function to confirm and store output of math
+    // pass these funcs to increment
+  
+  componentWillMount() {
+    this.props.history.push(this.props.history.location.pathname);
+  }
+
   render() {
     return (
       <div>
