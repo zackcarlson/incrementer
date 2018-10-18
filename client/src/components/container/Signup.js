217a7a1ea530fa68abc39b748fe5c983da8e8d21
zackcarlson <zackt.carlson@gmail.com>
2018-10-17 19:14:33 -0700

    Add broswer history and convert to class component

--- client/src/components/container/Signup.js
+++ client/src/components/container/Signup.js
@@ -1,36 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const SignUp = function() {
-  return (
-    <section className="signup-container">
-      <div className="signup-form">
-        <form action="" method="post">
-        <h2>Sign Up</h2>
+class SignUp extends Component {
+
+  componentWillMount() {
+    this.props.history.push(this.props.history.location.pathname);
+  }
+  render() {
+    return (
+      <section className="signup-container">
+        <div className="signup-form">
+          <form action="" method="post">
+          <h2>Sign Up</h2>
+            <div className="form-group">
+              <input type="text" className="form-control" name="username" placeholder="Username" required="required"/>
+            </div>
+
+            <div className="form-group">
+              <input type="email" className="form-control" name="email" placeholder="Email Address" required="required"/>
+            </div>
+
+            <div className="form-group">
+              <input type="password" className="form-control" name="password" placeholder="Password" required="required"/>
+            </div>
+
+            <div className="form-group">
+              <input type="password" className="form-control" name="confirm_password" placeholder="Confirm Password" required="required"/>
+            </div> 
+
           <div className="form-group">
-            <input type="text" className="form-control" name="username" placeholder="Username" required="required"/>
+            <button type="submit" className="btn btn-primary btn-lg">Sign Up</button>
           </div>
-
-          <div className="form-group">
-            <input type="email" className="form-control" name="email" placeholder="Email Address" required="required"/>
-          </div>
-
-          <div className="form-group">
-            <input type="password" className="form-control" name="password" placeholder="Password" required="required"/>
-          </div>
-
-          <div className="form-group">
-            <input type="password" className="form-control" name="confirm_password" placeholder="Confirm Password" required="required"/>
-          </div> 
-
-        <div className="form-group">
-          <button type="submit" className="btn btn-primary btn-lg">Sign Up</button>
-        </div>
-        </form>
-      <div className="text-center">Already have an account? <NavLink exact to="/"><a href="#">Login here</a></NavLink></div>
-    </div>
-  </section>
-  );
-};
+          </form>
+        <div className="text-center">Already have an account? <NavLink exact to="/"><a href="#">Login here</a></NavLink></div>
+      </div>
+    </section>
+    );
+  }
+}
 
 export default SignUp;
